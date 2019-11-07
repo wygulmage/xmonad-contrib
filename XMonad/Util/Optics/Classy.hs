8c82daf9406d25c7fee44da0a7a714f818ca8113
wygulmage <wygulmage@users.noreply.github.com>
2019-11-07 17:14:47 -0500

    started cleanup.

--- XMonad/Util/Optics/Classy.hs
+++ XMonad/Util/Optics/Classy.hs
@@ -81,7 +81,10 @@ import Control.Category (id, (.))
 import Data.Functor ((<$>))
 import Data.Traversable (traverse)
 import XMonad.Util.Optics.Classy.Stack (HasFocus (..), HasZipper (..), HasWindows (..))
-import XMonad.Util.Optics.Classy.Workspace
+-- import XMonad.Util.Optics.Classy.Workspace
+import XMonad.Util.Optics.Classy.XConfig
+import XMonad.Util.Optics.Classy.XConf
+import XMonad.Util.Optics.Class.XState
 
 --- Types:
 import Data.Map (Map)
@@ -117,27 +120,6 @@ A few classes comprise multiple optics: `HasBorder` has `_focusedBorder` and `_n
 -- Many of these are too polymorphic, because the types they represent in XMonad are too polymorphic. Please respect the types they /should/ have.
 
 class
-    ( HasXConfig ta ta Layout Layout
-    , HasButtonActions ta
-    , HasCurrentEvent ta
-    , HasDisplay ta
-    , HasBorder ta
-    , HasTheRoot ta
-    ) =>
-    HasXConf ta where
-    _xConf :: Simple Lens ta XConf
-
-class HasDisplay ta where
-    _display :: Simple Lens ta Display
-    default _display :: HasXConf ta => Simple Lens ta Display
-    _display = _xConf . O._display
-
-class HasTheRoot ta where
-    _theRoot :: Simple Lens ta Window
-    default _theRoot :: HasXConf ta => Simple Lens ta Window
-    _theRoot = _xConf . O._theRoot
-
-class
     (a ~ ScreenIdOf ta , b ~ ScreenIdOf tb) =>
     HasScreenId ta tb a b
     | ta -> a, tb -> b
@@ -155,120 +137,8 @@ class
     _screenDetail :: Lens ta tb a b
     -- Should be `Simple Lens ta ScreenDetail`
 
-class HasButtonActions ta where
-  _buttonActions :: Simple Lens ta (Map (KeyMask, Button) (Window -> X ()))
-  default _buttonActions ::
-      HasXConf ta => Simple Lens ta (Map (KeyMask, Button) (Window -> X ()))
-  _buttonActions = _xConf . O._buttonActions
-
-class
-     ( HasBorderColor ta
-     , HasHandleEventHook ta
-     , HasKeys ta
-     , HasLayoutHook ta tb a b
-     , HasManageHook ta
-     , HasModMask ta
-     , HasTerminal ta
-     , HasWorkspaceNames ta
-     ) =>
-     HasXConfig ta tb a b | ta -> a, tb -> b, ta b -> tb, tb a -> ta
-  where
-    _xConfig :: Lens ta tb (XConfig a) (XConfig b)
-    -- `_xConfig` instead of `_config` to (hopefully) avoid some confusion
-
-class HasCurrentEvent ta where
-    _currentEvent :: Simple Lens ta (Maybe Event)
-    default _currentEvent :: HasXConf ta => Simple Lens ta (Maybe Event)
-    _currentEvent = _xConf . O._currentEvent
-
-class HasBorder ta where
-    _focusedBorder, _normalBorder :: Simple Lens ta Pixel
-
-    default _focusedBorder :: HasXConf ta => Simple Lens ta Pixel
-    _focusedBorder = _xConf . O._focusedBorder
-
-    default _normalBorder :: HasXConf ta => Simple Lens ta Pixel
-    _normalBorder = _xConf . O._normalBorder
-
-class HasKeyActions ta where
-    _keyActions :: Simple Lens ta (Map (KeyMask, KeySym) (X ()))
-    default _keyActions ::
-        HasXConf ta => Simple Lens ta (Map (KeyMask, KeySym) (X ()))
-    _keyActions = _xConf . O._keyActions
-
-class HasMouseFocused ta where
-    _mouseFocused :: Simple Lens ta Bool
-    default _mouseFocused :: HasXConf ta => Simple Lens ta Bool
-    _mouseFocused = _xConf . O._mouseFocused
-
-class HasMousePosition ta where
-    _mousePosition :: Simple Lens ta (Maybe (Position, Position))
-    default _mousePosition :: HasXConf ta => Simple Lens ta (Maybe (Position, Position))
-    _mousePosition = _xConf . O._mousePosition
-
-class HasBorderColor ta where
-    _focusedBorderColor, _normalBorderColor :: Simple Lens ta String
-
-    default _focusedBorderColor ::
-        HasXConfig ta ta l l => Simple Lens ta String
-    _focusedBorderColor = _xConfig . O._focusedBorderColor
-
-    default _normalBorderColor ::
-        HasXConfig ta ta l l => Simple Lens ta String
-    _normalBorderColor = _xConfig . O._normalBorderColor
-
-class HasKeys ta where
-    _keys ::
-        Simple Lens ta (XConfig Layout -> Map (ButtonMask, KeySym) (X ()))
-
-class HasHandleEventHook ta where
-    _handleEventHook :: Simple Lens ta (Event -> X All)
-
-class
-    -- (a ~ ALayoutOf ta, b ~ ALayoutOf tb) =>
-    HasLayoutHook ta tb a b
-    | ta -> a, tb -> b, ta b -> tb, tb a -> ta
-  where
-    _layoutHook :: Lens ta tb (a Window) (b Window)
-
-class HasManageHook ta where
-    _manageHook :: Simple Lens ta ManageHook
-    default _manageHook :: HasXConfig ta ta l l => Simple Lens ta ManageHook
-    _manageHook = _xConfig . O._manageHook
-
-class HasModMask ta where
-    _modMask :: Simple Lens ta KeyMask
-    default _modMask :: HasXConfig ta ta l l => Simple Lens ta KeyMask
-    _modMask = _xConfig . O._modMask
-
-class HasTerminal ta where
-    _terminal :: Simple Lens ta String
-    default _terminal :: HasXConfig ta ta l l => Simple Lens ta String
-    _terminal = _xConfig . O._terminal
-
-class HasDragging ta where
-    _dragging :: Simple Lens ta (Maybe (Position -> Position -> X (), X ()))
-
-class HasExtensibleState ta where
-    _extensibleState ::
-        Simple Lens ta (Map String (Either String StateExtension))
-
-class HasMapped ta where
-    _mapped :: Simple Lens ta (Set Window)
-    _waitingUnmap :: Simple Lens ta (Map Window Int)
-
-class HasNumberLockMask ta where
-    _numberlockMask :: Simple Lens ta KeyMask
-
 class HasWindowSet ta where
     _windowset :: Simple Lens ta WindowSet
-
-class HasWorkspaceNames ta where
-  -- `_workspaceNames` is used so that `_workspaces` can be the Traversal of Workspaces.
-  _workspaceNames :: Simple Lens ta [WorkspaceId]
-  default _workspaceNames ::
-      HasXConfig ta ta l l => Simple Lens ta [WorkspaceId]
-  _workspaceNames = _xConfig . _workspaceNames
 
 class
     (a ~ ScreenOf ta, b ~ ScreenOf tb) =>
@@ -297,82 +167,14 @@ class
 
 ----- XMonad.Core -----
 
---- XConf instances:
--- XConf is not parameterized.
 
-instance HasXConf XConf where
-    _xConf = id
-
-instance HasXConfig XConf XConf Layout Layout where
-    _xConfig = O._xConfig
-
-instance HasButtonActions XConf
-instance HasCurrentEvent XConf
-instance HasDisplay XConf
-instance HasBorder XConf
-instance HasKeyActions XConf
-instance HasMouseFocused XConf
-instance HasMousePosition XConf
-instance HasTheRoot XConf
-instance HasBorderColor XConf
-instance HasHandleEventHook XConf
-instance HasKeys XConf
-instance HasLayoutHook XConf XConf Layout Layout
-instance HasLayouts XConf XConf (Layout Window) (Layout Window)
-instance HasLayout XConf XConf (Layout Window) (Layout Window)
-instance HasManageHook XConf
-instance HasModMask XConf
-instance HasTerminal XConf
-instance HasWorkspaceNames XConf
 
 --- XConfig instances:
 -- XConfig is parameterized by layout.
 
-instance HasXConfig (XConfig layout) (XConfig layout') layout layout' where
-    _xConfig = id
-
-instance HasBorderColor (XConfig layout)
-
-instance HasHandleEventHook (XConfig layout) where
-    _handleEventHook = O._handleEventHook
-
-instance HasKeys (XConfig layout) where
-    _keys = O._keys
-
-instance HasLayoutHook
-    (XConfig layout) (XConfig layout')
-    layout layout'
-  where
-    _layoutHook = O._layoutHook
-
-instance HasManageHook (XConfig layout) where
-    _manageHook = O._manageHook
-
-instance HasModMask (XConfig layout) where
-    _modMask = O._modMask
-
-instance HasTerminal (XConfig layout) where
-    _terminal = O._terminal
-
-instance HasWorkspaceNames (XConfig layout) where
-    _workspaceNames = O._workspaceNames
-
 
 --- XState instances:
 -- XState is not parameterized, and instantiates layout as Layout (in WindowSet).
-
-instance HasDragging XState where
-    _dragging = O._dragging
-
-instance HasExtensibleState XState where
-    _extensibleState = O._extensibleState
-
-instance HasMapped XState where
-    _mapped = O._mapped
-    _waitingUnmap = O._waitingUnmap
-
-instance HasNumberLockMask XState where
-    _numberlockMask = O._numberlockMask
 
 instance HasWindowSet XState where
     _windowset = O._windowset
